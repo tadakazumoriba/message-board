07511d34781b567022ab36c7ed91dffbba707350
tadakazumoriba <tadakazumoriba@gmail.com>
2016-10-01 03:55:25 +0000

    challenge2

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,5 +3,7 @@ class Message < ActiveRecord::Base
   validates :name , length: { maximum: 20 } , presence: true
   #内容は必須入力かつ2文字以上30文字以下
   validates :body , length: { minimum: 2, maximum: 30 } , presence: true
+  # 数値であり、0以上の場合有効
+  validates :age, numericality: {only_integer: true, greater_than: 0, less_than_or_equal_to: 150}
   
 end
