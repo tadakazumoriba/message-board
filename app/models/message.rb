f20e4f911679ed5e253a3e250fe48605d3cf7036
tadakazumoriba <tadakazumoriba@gmail.com>
2016-10-03 06:31:19 +0000

    change validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,6 +4,6 @@ class Message < ActiveRecord::Base
   #内容は必須入力かつ2文字以上30文字以下
   validates :body , length: { minimum: 2, maximum: 30 } , presence: true
   # 数値であり、0以上の場合有効
-  validates :age, numericality: {only_integer: true, greater_than: 0, less_than_or_equal_to: 150}
+  validates :age, numericality: {only_integer: true, greater_than_or_equal: 0, less_than_or_equal_to: 150}
   
 end
